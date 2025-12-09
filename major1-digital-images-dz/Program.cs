78c1a6224ac11c7ab04b6e3c61c07aed8e254ee3
Alexey <91151767+Azriel230@users.noreply.github.com>
2025-12-09 12:29:45 +0300

    init project

---
+++ major1-digital-images-dz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace major1_digital_images_dz
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
